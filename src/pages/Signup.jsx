a3797d802618384b122ea19f3829cb92f87ba32e
hammadqureshi31 <hammad2004qureshi@gmail.com>
2024-09-05 00:53:30 +0500

    user image bug fix

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -28,7 +28,7 @@ const Signup = () => {
         email,
         password,
         role,
-        profilePicture: downloadURL,
+        profilePicture: downloadURL || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
       };
 
       await axios.post(`${backendPortURL}user/signup`, formData);
